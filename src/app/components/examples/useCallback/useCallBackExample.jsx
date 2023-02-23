5495ca0697b19f88360fedc760f6f62f07fafe39
YuriyMitrofanov <yurasell@yandex.ru>
2023-02-23 14:24:25 +0300

    After useMemo() and useCallback()

--- src/app/components/examples/useCallback/useCallBackExample.jsx
+++ src/app/components/examples/useCallback/useCallBackExample.jsx
@@ -1,12 +1,74 @@
-import React from "react";
+import React, { useEffect, useRef, useState, useCallback } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
 const UseCallBackExample = () => {
+    // Для илллюстрации работы зададим состояние для переменной data. Это будет объект.
+    const [data, setData] = useState({});
+
+    // Рендеринг без Callback
+    const withoutCallback = useRef(0);
+    // Функция-обработчик поля ввода, которая принимает в себя значение event.target и записывает значения с соответствующими клбчами
+    // в объект с исходными данными data.
+    const handleChange = ({ target }) => {
+        setData(prevState => ({ ...prevState, [target.name]: target.value }));
+    };
+    // В нашем примере нужно продемонстрировать зависимость рендеринга компонента от работы функции, поэтому мы не будем делать useEffect(() => {},[data]),
+    // А пропишем функцию, которая будет получать data в качестве аргумента т.е. выводить значение data каждый раз при обновлении состояния data.
+    const validateVithoutCallback = (data) => {
+        console.log(data);
+    };
+    // Далее уже с помощью useEffect(() => {},[validateVithoutCallback]) мы отследим каждый рендеринг функции validateVithoutCallback и установим счетчик
+    // на количество изменений этой функции. Значение данного счетчика отобразим над полем ввода <p>{withoutCallback.current}</p>. В текущем состоянии
+    // при вводе каждого символа в поле ввода производится ререндеринг функции и счетчик это показывает, увеличением значения на +1.
+    useEffect(() => {
+        withoutCallback.current++;
+    }, [validateVithoutCallback]);
+
+    // Рендеринг с Callback
+    // Теперь проиллюстрируем работу хука useCallback() на примере аналогичной функции
+    // Зададим начальное состояние параметра ref для withCallback
+    const withCallback = useRef(0);
+    // Зададим функфцию по аналогии с предыдущим методом
+    const validateVithCallback = useCallback((data) => {
+        console.log(data);
+    }, []);
+    // И создадим счетчик референсов для параметра withCallback
+    useEffect(() => {
+        withCallback.current++;
+    }, [validateVithCallback]);
+
+    // В данном случае мы выводим в консоль записанный в поле ввода с помощью обоих методов результат.
+    useEffect(() => {
+        validateVithoutCallback(data);
+        validateVithCallback(data);
+    }, [data]);
+
+    // Результат говорит о том, что при вводе символов в поле ввода счетчик without callback увеличивает значение в соответствии с увеличением
+    // количества введенных символов, в отлиие от with callback, кторый показывает, что наша функция отрендерилась 1 раз.
+    // Таким образом useCallback - служит некой оболочкой которая кэширует нашу функцию.
+    // Конструкция useCallback идентична useMemo, но отличие в том, что useCallback хранит в себе целую функцию, а useMemo - лишь значение (данные), получаемое
+    // в результате работы функции при неизменности зависимости
+
     return (
         <CardWrapper>
             <SmallTitle>Example</SmallTitle>
+            <p>Render without callback: {withoutCallback.current}</p>
+            <p>Render with callback: {withCallback.current}</p>
+            <label htmlFor="email" className="form-label">
+                Email{" "}
+            </label>
+            <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value = {data.email || ""} // в нашем случае поле не контролируемое т.к. в useState({}) не задано значение по умолчанию email: ""
+                // т.е. если data.email есть, то отображается он, в противном случае - пустая строка
+                onChange = {handleChange}
+            />
         </CardWrapper>
+
     );
 };
 
--- src/app/components/examples/useMemo/complexCalculate.jsx
+++ src/app/components/examples/useMemo/complexCalculate.jsx
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
+// Для иммитаации сложных вычислений создадим функцию
+function factorial(n) {
+    return n
+        ? n * factorial(n - 1)
+        : 1;
+};
+
+function runFactorial(n) {
+    console.log("run factorial");
+    return factorial(n);
+};
+// В нашем примере факториал вычисляется в зависимости от значения n - числа слогаемых факториала
+// Значение n управляется кнопками Increment (увеличивает n на 10) и Decrement (уменьшает n на 10)
+// Каждый раз при нажатии на кнопку у нас производится вычисление и обновляется состояние т.е.
+// происходит ререндеринг компонента. И все бы хорошо, но на рендеринг компонента могут влиять
+// и другие состояния. ЧТО ТЕПЕРЬ ПРИ ВЫЗОВЕ КАЖДОГО ОБНОВЛЕНИЯ СТРАНИЦЫ ЭТОТ ФАКТОРИАЛ ВЫЧИСЛЯТЬ?
+// Для иллюстрации создадим новое состояник, кторое упаравляется кнопкой.
 const ComplexCalculateExample = () => {
+    // Состояние вычисления факториала
+    const [value, setValue] = useState(100);
+    // const fact = runFactorial(value); // Запишем результат вычислений в переменную
+
+    // Пример альтернативного состояния, влияющего на ререндеринг всего компонента Кнопка Change color
+    // вызывает функцию, которая задает цвет кнопки в зависимости отзначения состояния otherState
+    // (true / false). Данную функцию мы вызываем внутри className самой кнопки
+    // т.о. просто меняя цвет кнопки мы заново ререндерим компонент и попутно вызываем расчет
+    // факториала, что очень сильно нагружает системму
+    const [otherState, setOtherState] = useState(false);
+    const buttonColor = otherState ? "primary" : "secondary";
+    useEffect(() => console.log("render button color"), [buttonColor]);
+    // Для предотвращения проведения лишних вычислений испльзуем хук useMemo. Он сохраняет вычисления
+    // внутри компонента и при повторном рендеринге проверяет изменились ли связи у данного состояния
+    // и если нет, то значение вычисления берется из памяти, если связи поменялись (например изменились
+    // какие-либо входящие данные из-за влияния другого состояния), то производится повторное вычисление
+    // Первым параметром в useMemo передается функция, вторым - массив зависимостей (как в useEffect), т.е.
+    // изменение какого параметра приводит к ререндерингу.
+    // useMemo(() => runFactorial(аргумент), [массив зависимостей]);
+    // и перепишем значение работы функции следующим образом:
+    const fact = useMemo(() => runFactorial(value), [value]);
+    // Использование данной конструкции позволяет кэшировать результат работы функции и не запускать
+    // функцию при каждом рендеринге компонента. Но чрезмерное использование этого хука может привести
+    // замедлению работы. И по возможности нужно обходиться другими методами оптимизации.
     return (
         <>
             <CardWrapper>
                 <SmallTitle>Кэширование сложных вычислений</SmallTitle>
+                <p>value: {value}</p>
+                <p>result: {fact}</p>
+                <button
+                    className="btn btn-primary mx-2"
+                    onClick={() => setValue(prevState => prevState + 10)}
+                >Increment</button>
+                <button
+                    className="btn btn-primary mx-2"
+                    onClick={() => setValue(prevState => prevState - 10)}
+                >Decrement</button>
             </CardWrapper>
             <CardWrapper>
                 <SmallTitle>Зависимость от сторонних setState</SmallTitle>
+                <button
+                    className={"btn ms-md-2 btn-" + buttonColor}
+                    onClick={() => setOtherState(prevState => !prevState)}
+                >Change color</button>
             </CardWrapper>
         </>
     );
